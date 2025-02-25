fb87538aa88e43ab1fff1bed273ec3751129ebbe
joltcops <pracheta@kgpian.iitkgp.ac.in>
2025-02-25 22:52:37 +0530

    Adding household serializer

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import citizen
+from .models import household
 
+admin.site.register(household)
 admin.site.register(citizen)  # Register the citizen model
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer
+from .models import household
 from .models import citizen
 
 class citizenSerializer(ModelSerializer):
     class Meta:
         model = citizen
         fields = '__all__' 
+
+class householdSerializer(ModelSerializer):
+    class Meta:
+        model = household
+        fields = '__all__'
